15f9a0c0c9b1ee3a7d18cc02595ea4c6bcdc01e5
Sync1983 <sync06@mail.ru>
2018-02-24 20:37:22 +0300

    first commit

---
+++ basic/controllers/IncomController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\controllers;
+
+class IncomController extends \yii\web\Controller {
+  public function actionLoad() {
+    $this->layout = "dummy.php";
+    $data = ["get"=> $_SERVER['QUERY_STRING']];
+    return $this->render('load',$data);
+  }
+}
